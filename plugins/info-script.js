9745559f3409983354ec8226c116e4d7506d2d68
NaufalID <88891630+NaufalID@users.noreply.github.com>
2022-09-29 11:33:12 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,26 +2,26 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://www.youtube.com/channel/UC1esKQw-DKM5Xa5hF7vqdGA*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
 https://github.com/ImYanXiao/Elaina-MultiDevice
 
 Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtu.be/Pgv_GuQTPyo
+https://www.youtube.com/channel/UC1esKQw-DKM5Xa5hF7vqdGA
 
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
 
-wa.me/6282127487538`
+wa.me/6289601124991`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/nauffhdkz",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://Instagram.com/nauffhdkz", 
+    title: 'WHatsapp-Bot MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
